6230a84341b2ffe76bd1a8eb7a33838a470643cd
Anderson Danilo <contato@andersondanilo.com>
2020-07-10 16:27:46 -0300

    first commit

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM mongo:4.2
+
+COPY docker-entrypoint.sh /usr/local/bin/
---
+++ docker-entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+/usr/bin/mongod --bind_ip_all --port ${PORT} --replSet ${RS}
+
+echo "Waiting for startup.."
+until mongo --host 127.0.0.1:${PORT} --eval 'quit(db.runCommand({ ping: 1 }).ok ? 0 : 2)' &>/dev/null; do
+      printf '.'
+        sleep 1
+    done
+
+    echo "Started.."
+
+    echo setup.sh time now: `date +"%T" `
+    mongo --host 127.0.0.1:${PORT} <<EOF
+    var cfg = {
+        "_id": "${RS}",
+        "protocolVersion": 1,
+        "members": [
+            {
+                "_id": 0,
+                "host": "127.0.0.1:${PORT}",
+                "priority": 2
+            }
+        ]
+    };
+    rs.initiate(cfg, { force: true });
+    rs.reconfig(cfg, { force: true });
+EOF
+
+echo "ReplicaSet configured, tailing log"
+
+tail -f /var/log/mongodb/mongod.log
