42a53e98df0fc6a3b7d3541d74b52191f3470a2b
Anderson Danilo <contato@andersondanilo.com>
2020-07-10 23:03:07 -0300

    public host config

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-/usr/bin/mongod --bind_ip_all --port ${PORT} --replSet ${RS} &
+mkdir /data/db/repl1
+mkdir /data/db/repl2
+
+/usr/bin/mongod --bind_ip_all --port 27017 --replSet ${RS} --dbpath=/data/db/repl1 &
+/usr/bin/mongod --bind_ip_all --port 27018 --replSet ${RS} --dbpath=/data/db/repl2 &
 
 echo "Waiting for startup.."
-until mongo --host 127.0.0.1:${PORT} --eval 'quit(db.runCommand({ ping: 1 }).ok ? 0 : 2)' &>/dev/null; do
+
+until mongo --host 127.0.0.1:27017 --eval 'quit(db.runCommand({ ping: 1 }).ok ? 0 : 2)' &>/dev/null; do
       printf '.'
         sleep 1
     done
@@ -11,14 +16,18 @@ until mongo --host 127.0.0.1:${PORT} --eval 'quit(db.runCommand({ ping: 1 }).ok 
     echo "Started.."
 
     echo setup.sh time now: `date +"%T" `
-    mongo --host 127.0.0.1:${PORT} <<EOF
+    mongo --host 127.0.0.1:27017 <<EOF
     var cfg = {
         "_id": "${RS}",
         "protocolVersion": 1,
         "members": [
             {
                 "_id": 0,
-                "host": "${REPL1}:${PORT}",
+                "host": "${HOSTNAME}:27017",
+            },
+            {
+                "_id": 1,
+                "host": "${HOSTNAME}:27018",
             }
         ]
     };
